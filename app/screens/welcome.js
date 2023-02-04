bb6a8e6230e18b144c42bd5017024922346072e3
Aditi-Devi-Mobile <aditi@devimobile.app>
2023-02-04 14:00:50 -0500

    Added screens folder and starte

---
+++ app/screens/welcome.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Text, Image, StyleSheet, TouchableOpacity } from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+
+    },
+    
+});
+
+export default function WelcomePage ({navigation}) {
+    return (
+        <View style = {styles.container}>
+
+        </View>
+    )
+}
+
