69f6b42a528e30da9aaa797c3eb45b8fd97e7400
Aditi-Devi-Mobile <aditi@devimobile.app>
2023-02-04 17:33:44 -0500

    Added navigation for screens

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import WelcomePage from './app/screens/welcome.js';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName = "Welcome">
+        <Stack.Screen name = "Welcome" component = {WelcomePage} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
