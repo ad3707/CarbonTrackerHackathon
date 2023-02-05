dd7e4a6751895a8547ed016f9eab1d51eaae85a3
Aditi-Devi-Mobile <aditi@devimobile.app>
2023-02-04 19:06:00 -0500

    added buttons to questions

--- App.js
+++ App.js
@@ -2,10 +2,10 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import WelcomePage from './app/screens/welcome.js';
 
-import waterQ1 from './app/screens/waterQuestions/question1.js'
-import waterQ2 from './app/screens/waterQuestions/question2.js'
-import waterQ3 from './app/screens/waterQuestions/question3.js'
-import waterQ4 from './app/screens/waterQuestions/question4.js'
+import WaterQ1 from './app/screens/waterQuestions/question1.js'
+import WaterQ2 from './app/screens/waterQuestions/question2.js'
+import WaterQ3 from './app/screens/waterQuestions/question3.js'
+import WaterQ4 from './app/screens/waterQuestions/question4.js'
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
@@ -15,12 +15,12 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName = "waterQ1">
+      <Stack.Navigator initialRouteName = "WaterQ1">
         <Stack.Screen name = "Welcome" component = {WelcomePage} />
-        <Stack.Screen name = "waterQ1" component = {waterQ1} />
-        <Stack.Screen name = "waterQ2" component = {waterQ2} />
-        <Stack.Screen name = "waterQ3" component = {waterQ3} />
-        <Stack.Screen name = "waterQ4" component = {waterQ4} />
+        <Stack.Screen name = "WaterQ1" component = {WaterQ1} />
+        <Stack.Screen name = "WaterQ2" component = {WaterQ2} />
+        <Stack.Screen name = "WaterQ3" component = {WaterQ3} />
+        <Stack.Screen name = "WaterQ4" component = {WaterQ4} />
       </Stack.Navigator>
     </NavigationContainer>
   );
