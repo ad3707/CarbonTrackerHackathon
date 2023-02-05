6b800ad79594ba7172df4f2499d5c9e8b2cd7f29
Aditi-Devi-Mobile <aditi@devimobile.app>
2023-02-04 20:43:49 -0500

    Added option to hide header

--- App.js
+++ App.js
@@ -4,15 +4,15 @@ import WelcomePage from './app/screens/welcome.js';
 import SignUpPage from './app/screens/signup.js';
 import ProfilePage from './app/screens/profile.js';
 
-import WaterQ1 from './app/screens/waterQuestions/question1.js'
-import WaterQ2 from './app/screens/waterQuestions/question2.js'
-import WaterQ3 from './app/screens/waterQuestions/question3.js'
-import WaterQ4 from './app/screens/waterQuestions/question4.js'
+import WaterQ1 from './app/screens/waterQuestions/question1.js';
+import WaterQ2 from './app/screens/waterQuestions/question2.js';
+import WaterQ3 from './app/screens/waterQuestions/question3.js';
+import WaterQ4 from './app/screens/waterQuestions/question4.js';
 
-import WasteQ1 from './app/screens/waste/question1W.js'
-import WasteQ2 from './app/screens/waste/question2W.js'
-import WasteQ3 from './app/screens/waste/question3W.js'
-import WasteQ4 from './app/screens/waste/question4W.js'
+import WasteQ1 from './app/screens/waste/question1W.js';
+import WasteQ2 from './app/screens/waste/question2W.js';
+import WasteQ3 from './app/screens/waste/question3W.js';
+import WasteQ4 from './app/screens/waste/question4W.js';
 
 import HomeEnergyQ1 from './app/screens/homeEnergy/question1HE.js';
 import HomeEnergyQ2 from './app/screens/homeEnergy/question2HE.js';
@@ -33,7 +33,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName = "Home Energy Question 1">
-        <Stack.Screen name = "Welcome" component = {WelcomePage} />
+        <Stack.Screen name = "Welcome" component = {WelcomePage} options={{headerShown: false}}/>
         <Stack.Screen name = "Water Question 1" component = {WaterQ1} />
         <Stack.Screen name = "Water Question 2" component = {WaterQ2} />
         <Stack.Screen name = "Water Question 3" component = {WaterQ3} />
@@ -42,16 +42,16 @@ export default function App() {
         <Stack.Screen name = "Waste Question 2" component = {WasteQ2} />
         <Stack.Screen name = "Waste Question 4" component = {WasteQ4} />
         <Stack.Screen name = "Waste Question 3" component = {WasteQ3} />
-        <Stack.Screen name = "Home Energy Question 1" component = {HomeEnergyQ1} />
-        <Stack.Screen name = "Home Energy Question 2" component = {HomeEnergyQ2} />
-        <Stack.Screen name = "Home Energy Question 3" component = {HomeEnergyQ3} />
-        <Stack.Screen name = "Home Energy Question 4" component = {HomeEnergyQ4} />
+        <Stack.Screen name = "Home Energy Question 1" component = {HomeEnergyQ1} options={{headerShown: false}}/>
+        <Stack.Screen name = "Home Energy Question 2" component = {HomeEnergyQ2} options={{headerShown: false}} />
+        <Stack.Screen name = "Home Energy Question 3" component = {HomeEnergyQ3} options={{headerShown: false}} />
+        <Stack.Screen name = "Home Energy Question 4" component = {HomeEnergyQ4} options={{headerShown: false}} />
         <Stack.Screen name = "Transportation Question 1" component = {TransportationQ1} />
         <Stack.Screen name = "Transportation Question 2" component = {TransportationQ2} />
         <Stack.Screen name = "Transportation Question 3" component = {TransportationQ3} />
         <Stack.Screen name = "Transportation Question 4" component = {TransportationQ4} />
-        <Stack.Screen name = "SignUp" component = {SignUpPage} />
-        <Stack.Screen name = "Profile" component = {ProfilePage} />
+        <Stack.Screen name = "SignUp" component = {SignUpPage} options={{headerShown: false}} />
+        <Stack.Screen name = "Profile" component = {ProfilePage} options={{headerShown: false}}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
